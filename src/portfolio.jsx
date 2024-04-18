ae23e25b7a99e40802482828975542ba026a1416
Abdurrehman211 <abidmani790@gmail.com>
2024-04-18 23:18:35 +0500

    Add Navbar component and styling

---
+++ src/portfolio.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+function Portfolio(){
+return (
+    <>
+    <div className="header">
+        <Navbar/>
+    </div>
+    <div className="main">
+       
+    </div>
+    </>
+)
+}
+export default Portfolio;
